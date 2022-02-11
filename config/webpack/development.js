ea30dd611dd90b1d33b3181971dcf40ae19b6c93
otegami <a.s.takuya1026@gmail.com>
2022-02-11 10:41:44 +0900

    webpacker の設定項目

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+Object.defineProperty(RegExp.prototype, 'toJSON', {
+  value: RegExp.prototype.toString,
+})
+const fs = require('fs')
+fs.writeFileSync('./resultToWebpackConfig.txt', JSON.stringify(environment.toWebpackConfig()))
